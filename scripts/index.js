35b7a685a7703d9ae960a8c60f74b6a3582424d7
Ananya Mohanty <42179851+ananya-mohanty@users.noreply.github.com>
2020-01-19 15:45:16 +0530

    updated index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const addBtn = document.getElementById("add-btn");
+var m=0;
 const addTxt = document.getElementById("add-txt");
 const titles = document.getElementById("titles");
 const titleMessage = document.getElementById("title-message");
@@ -6,11 +7,38 @@ addBtn.addEventListener('click', handleAdd);
 let i = localStorage.length;
 updateList();
 
-function handleAdd(e){
+function handleAdd(e,m){
+
     e.preventDefault();
-    localStorage.setItem(i, addTxt.value);
-    insertCard(addTxt.value);
-    i++;
+    
+    for (var j in localStorage) {
+        if (localStorage[j] === addTxt.value) {
+            //alert('no');
+            m=1;
+        }
+    }
+    //  duplicate();
+    
+    // function duplicate(req, res){
+        if(m==1){
+            //alert('no');
+           
+            document.getElementById("dupli").innerHTML ="<div class='alert alert-danger'>Title already exists. </div>";
+            document.getElementById("success").innerHTML ="";
+
+        //   req.flash("error", "Title already exits.");
+        }
+        else{
+            document.getElementById("success").innerHTML ="<div class='alert alert-success'> Title added successfully </div>";
+            document.getElementById("dupli").innerHTML ="";
+           //req.flash("success", "Title successfully added");
+            localStorage.setItem(i, addTxt.value);
+            insertCard(addTxt.value);
+            i++;
+        }
+    // }
+   // return m;
+    
     console.log(localStorage);
 }
 
